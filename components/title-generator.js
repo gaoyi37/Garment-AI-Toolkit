7a1d62af7fb56866bc7e966e31ea57a8f262dcb4
gaoyi37 <gaoyiyang37@gmail.com>
2026-06-01 01:40:24 +0800

    Create title-generator.js

---
+++ components/title-generator.js
@@ -0,0 +1,11 @@
+export function generateTitle(productName) {
+    return `Optimized title for: ${productName}`;
+}
+
+export function generateSellingPoints(productName) {
+    return [
+        `Selling point 1 for ${productName}`,
+        `Selling point 2 for ${productName}`,
+        `Selling point 3 for ${productName}`,
+    ];
+}

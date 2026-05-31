648a1f7fe5f2579477eefe01c6c547031d562bec
gaoyi37 <gaoyiyang37@gmail.com>
2026-06-01 01:39:29 +0800

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,8 @@
+console.log("Garment AI Toolkit is running");
+
+function generateTitle(productName) {
+    return `Optimized title for: ${productName}`;
+}
+
+// 测试
+console.log(generateTitle("夏季男士短袖"));
